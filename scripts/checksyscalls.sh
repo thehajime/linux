04cb554bfdc7b605c91e7db919ef8e2ac1d69149
Akira Moroo <retrage01@gmail.com>
2019-09-19 17:28:19 +0900

    Supress clone3 not implemented warning
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- scripts/checksyscalls.sh
+++ scripts/checksyscalls.sh
@@ -40,6 +40,10 @@ cat << EOF
 #define __IGNORE_setrlimit	/* setrlimit */
 #endif
 
+#ifndef __ARCH_WANT_SYS_CLONE3
+#define __IGNORE_clone3		/* clone3 */
+#endif
+
 /* Missing flags argument */
 #define __IGNORE_renameat	/* renameat2 */
 
