a7c92a709f3b09839e36cbb86b3a8fe771d3e055
Octavian Purdila <tavi.purdila@gmail.com>
2019-09-24 14:15:03 +0900

    um lkl: system call interface and application API
    The LKL application API is based on the kernel system call interface
    in order to offer a stable API to applications. Note that we can't
    offer the full Linux system call interface due to LKL limitations such
    as lack of virtual memory, signal, user processes, etc.
    
    The host is using the LKL interrupt mechanism (lkl_trigger_irq) to
    initiate a system call. The system call is executed in the context of
    the init process.
    
    To avoid collisions between the Linux API and the LKL API (e.g.  struct
    stat, MKNOD, etc.) we use a python script to modify the user headers
    and to prefix all of the global symbols (structures, typedefs,
    defines) with LKL, lkl, _LKL, _lkl, __LKL or __lkl.
    
    Cc: Conrad Meyer <cem@FreeBSD.org>
    Cc: Jens Staal <staal1978@gmail.com>
    Cc: Lai Jiangshan <jiangshanlai@gmail.com>
    Cc: Luca Dariz <luca.dariz@gmail.com>
    Cc: Michael Zimmermann <sigmaepsilon92@gmail.com>
    Cc: Patrick Collins <pscollins@google.com>
    Cc: Pierre-Hugues Husson <phh@phh.me>
    Cc: Yuan Liu <liuyuan@google.com>
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>
    Signed-off-by: Octavian Purdila <tavi.purdila@gmail.com>

---
+++ arch/um/lkl/include/uapi/asm/unistd.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+#define __ARCH_WANT_SYSCALL_NO_AT
+#define __ARCH_WANT_SYSCALL_DEPRECATED
+#define __ARCH_WANT_SYSCALL_NO_FLAGS
+#define __ARCH_WANT_RENAMEAT
+#define __ARCH_WANT_NEW_STAT
+#define __ARCH_WANT_SET_GET_RLIMIT
+#define __ARCH_WANT_TIME32_SYSCALLS
+
+#include <asm/bitsperlong.h>
+
+#if __BITS_PER_LONG == 64
+#define __ARCH_WANT_SYS_NEWFSTATAT
+#endif
+
+#include <asm-generic/unistd.h>
