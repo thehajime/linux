c397b1d8d7e0844d4744f9824d780664164bed74
Octavian Purdila <tavi.purdila@gmail.com>
2019-09-24 14:13:54 +0900

    um lkl: interrupt support
    Add APIs that allows the host to reserve and free and interrupt number
    and also to trigger an interrupt.
    
    The trigger operation will simply store the interrupt data in
    queue. The interrupt handler is run later, at the first opportunity it
    has to avoid races with any kernel threads.
    
    Currently, interrupts are run on the first interrupt enable operation
    if interrupts are disabled and if we are not already in interrupt
    context.
    
    When triggering an interrupt, it uses GCC's built-in functions for
    atomic memory access to synchronize and simple boolean flags.
    
    Cc: Michael Zimmermann <sigmaepsilon92@gmail.com>
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>
    Signed-off-by: Octavian Purdila <tavi.purdila@gmail.com>

---
+++ arch/um/lkl/include/asm/irq.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_LKL_IRQ_H
+#define _ASM_LKL_IRQ_H
+
+#define IRQ_STATUS_BITS		(sizeof(long) * 8)
+#define NR_IRQS			((int)(IRQ_STATUS_BITS * IRQ_STATUS_BITS))
+
+void run_irqs(void);
+void set_irq_pending(int irq);
+
+#include <uapi/asm/irq.h>
+
+#endif
