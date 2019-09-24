486a1044bfa3de6a49462624610d9dfb925d88c7
Octavian Purdila <tavi.purdila@gmail.com>
2019-09-24 15:20:36 +0900

    lkl tools: virtio: add network device support
    This also adds various virtio_net backend to be used as network devices.
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>
    Signed-off-by: H.K. Jerry Chu <hkchu@google.com>
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>
    Signed-off-by: Motomu Utsumi <motomuman@gmail.com>
    Signed-off-by: Patrick Collins <pscollins@google.com>
    Signed-off-by: Thomas Liebetraut <thomas@tommie-lie.de>
    Signed-off-by: Xiao Jia <xiaoj@google.com>
    Signed-off-by: Yuan Liu <liuyuan@google.com>
    Signed-off-by: Octavian Purdila <tavi.purdila@gmail.com>

---
+++ tools/lkl/scripts/dpdk-sdk-build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-2.0
+
+dpdk_version="17.02"
+
+git clone -b v${dpdk_version} git://dpdk.org/dpdk dpdk-${dpdk_version}
+
+RTE_SDK=$(pwd)/dpdk-${dpdk_version}
+RTE_TARGET=$(uname -m)-native-linuxapp-gcc
+export RTE_SDK
+export RTE_TARGET
+export EXTRA_CFLAGS="-fPIC -O0 -g3"
+
+set -e
+cd dpdk-${dpdk_version}
+make -j1 T=${RTE_TARGET} config
+make -j3 \
+  || (echo "dpdk build failed" && exit 1)
