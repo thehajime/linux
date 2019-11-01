ab5f51d9d2a427d25620010d498c47514dd90183
Octavian Purdila <tavi.purdila@gmail.com>
2019-11-01 13:49:40 +0900

    lkl: initialization and cleanup
    Add the lkl_start_kernel and lkl_sys_halt APIs that start and
    respectively stops the Linux kernel.
    
    lkl_start_kernel creates a separate threads that will run the initial
    and idle kernel thread. It waits for the kernel to complete
    initialization before returning, to avoid races with system calls
    issues by the host application.
    
    During the setup phase, we create "/init" in initial ramfs root
    filesystem to avoid mounting the "real" rootfs since ramfs is good
    enough for now.
    
    lkl_stop_kernel will shutdown the kernel, terminate all threads and
    free all host resources used by the kernel before returning.
    
    This patch also introduces idle CPU handling since it is closely
    related to the shutdown process. A host semaphore is used to wait for
    new interrupts when the kernel switches the CPU to idle to avoid
    wasting host CPU cycles. When the kernel is shutdown we terminate the
    idle thread at the first CPU idle event.
    
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>
    Signed-off-by: Michael Zimmermann <sigmaepsilon92@gmail.com>
    Signed-off-by: Patrick Collins <pscollins@google.com>
    Signed-off-by: Yuan Liu <liuyuan@google.com>
    Signed-off-by: Octavian Purdila <tavi.purdila@gmail.com>

--- arch/um/lkl/include/uapi/asm/host_ops.h
+++ arch/um/lkl/include/uapi/asm/host_ops.h
@@ -17,7 +17,13 @@ struct lkl_jmp_buf {
  * These operations must be provided by a host library or by the application
  * itself.
  *
+ * @virtio_devices - string containg the list of virtio devices in virtio mmio
+ * command line format. This string is appended to the kernel command line and
+ * is provided here for convenience to be implemented by the host library.
+ *
  * @print - optional operation that receives console messages
+ *
+ * @panic - called during a kernel panic
  *
  * @sem_alloc - allocate a host semaphore an initialize it to count
  * @sem_free - free a host semaphore
@@ -78,7 +84,10 @@ struct lkl_jmp_buf {
  * @jmp_buf_longjmp - perform a jump back to the saved jump buffer
  */
 struct lkl_host_operations {
+	const char *virtio_devices;
+
 	void (*print)(const char *str, int len);
+	void (*panic)(void);
 
 	struct lkl_sem *(*sem_alloc)(int count);
 	void (*sem_free)(struct lkl_sem *sem);
@@ -120,6 +129,23 @@ struct lkl_host_operations {
 	void (*jmp_buf_set)(struct lkl_jmp_buf *jmpb, void (*f)(void));
 	void (*jmp_buf_longjmp)(struct lkl_jmp_buf *jmpb, int val);
 };
+
+/**
+ * lkl_start_kernel - registers the host operations and starts the kernel
+ *
+ * The function returns only after the kernel is shutdown with lkl_sys_halt.
+ *
+ * @lkl_ops - pointer to host operations
+ * @cmd_line - format for command line string that is going to be used to
+ * generate the Linux kernel command line
+ */
+int lkl_start_kernel(struct lkl_host_operations *lkl_ops, const char *cmd_line,
+		     ...);
+
+/**
+ * lkl_is_running - returns 1 if the kernel is currently running
+ */
+int lkl_is_running(void);
 
 int lkl_printf(const char *fmt, ...);
 void lkl_bug(const char *fmt, ...);
