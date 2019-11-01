64a5d6c94d331058331af7d191d2cdbe870d009b
Octavian Purdila <tavi.purdila@gmail.com>
2019-11-01 12:37:56 +0900

    lkl: architecture skeleton for Linux kernel library
    Adds the LKL Kconfig, vmlinux linker script, basic architecture
    headers and miscellaneous basic functions or stubs such as
    dump_stack(), show_regs() and cpuinfo proc ops.
    
    The headers we introduce in this patch are simple wrappers to the
    asm-generic headers or stubs for things we don't support, such as
    ptrace, DMA, signals, ELF handling and low level processor operations.
    
    The kernel configuration is automatically updated to reflect the
    endianness of the host, 64bit support or the output format for
    vmlinux's linker script. We do this by looking at the ld's default
    output format.
    
    Signed-off-by: Andreas Abel <aabel@google.com>
    Signed-off-by: Conrad Meyer <cem@FreeBSD.org>
    Signed-off-by: Edison M. Castro <edisonmcastro@hotmail.com>
    Signed-off-by: H.K. Jerry Chu <hkchu@google.com>
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>
    Signed-off-by: Jens Staal <staal1978@gmail.com>
    Signed-off-by: Lai Jiangshan <jiangshanlai@gmail.com>
    Signed-off-by: Levente Kurusa <levex@linux.com>
    Signed-off-by: Luca Dariz <luca.dariz@gmail.com>
    Signed-off-by: Mark Stillwell <mark@stillwell.me>
    Signed-off-by: Matthieu Coudron <mattator@gmail.com>
    Signed-off-by: Michael Zimmermann <sigmaepsilon92@gmail.com>
    Signed-off-by: Motomu Utsumi <motomuman@gmail.com>
    Signed-off-by: Patrick Collins <pscollins@google.com>
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>
    Signed-off-by: Pierre-Hugues Husson <phh@phh.me>
    Signed-off-by: Xiao Jia <xiaoj@google.com>
    Signed-off-by: Yuan Liu <liuyuan@google.com>
    Signed-off-by: Octavian Purdila <tavi.purdila@gmail.com>

---
+++ arch/um/lkl/include/asm/xor.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_LKL_XOR_H
+#define _ASM_LKL_XOR_H
+
+#include <asm-generic/xor.h>
+
+#define XOR_SELECT_TEMPLATE(x) (&xor_block_8regs)
+
+#endif /* _ASM_LKL_XOR_H */
