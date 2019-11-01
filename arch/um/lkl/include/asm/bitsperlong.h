62f78a316ce03a2e7e290e03bf07ff513fdb335b
Octavian Purdila <tavi.purdila@gmail.com>
2019-11-01 12:37:56 +0900

    um lkl: architecture skeleton for Linux kernel library
    Adds the LKL Kconfig, vmlinux linker script, basic architecture
    headers and miscellaneous basic functions or stubs such as
    dump_stack(), show_regs() and cpuinfo proc ops.
    
    The headers we introduce in this patch are simple wrappers to the
    asm-generic headers or stubs for things we don't support, such as
    ptrace, DMA, signals, ELF handling and low level processor operations.
    
    The kernel configuration is automatically updated to reflect the
    endianness of the host, 64bit support or the output format for
    vmlinux's linker script. We do this by looking at the ld's default
    output format.
    
    Cc: Andreas Abel <aabel@google.com>
    Cc: Conrad Meyer <cem@FreeBSD.org>
    Cc: Edison M. Castro <edisonmcastro@hotmail.com>
    Cc: H.K. Jerry Chu <hkchu@google.com>
    Cc: Jens Staal <staal1978@gmail.com>
    Cc: Lai Jiangshan <jiangshanlai@gmail.com>
    Cc: Levente Kurusa <levex@linux.com>
    Cc: Luca Dariz <luca.dariz@gmail.com>
    Cc: Mark Stillwell <mark@stillwell.me>
    Cc: Matthieu Coudron <mattator@gmail.com>
    Cc: Michael Zimmermann <sigmaepsilon92@gmail.com>
    Cc: Motomu Utsumi <motomuman@gmail.com>
    Cc: Patrick Collins <pscollins@google.com>
    Cc: Petros Angelatos <petrosagg@gmail.com>
    Cc: Pierre-Hugues Husson <phh@phh.me>
    Cc: Xiao Jia <xiaoj@google.com>
    Cc: Yuan Liu <liuyuan@google.com>
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>
    Signed-off-by: Octavian Purdila <tavi.purdila@gmail.com>

---
+++ arch/um/lkl/include/asm/bitsperlong.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __LKL_BITSPERLONG_H
+#define __LKL_BITSPERLONG_H
+
+#include <uapi/asm/bitsperlong.h>
+
+#define BITS_PER_LONG __BITS_PER_LONG
+
+#define BITS_PER_LONG_LONG 64
+
+#endif
---
+++ arch/um/lkl/include/asm/byteorder.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_LKL_BYTEORDER_H
+#define _ASM_LKL_BYTEORDER_H
+
+#include <uapi/asm/byteorder.h>
+
+#endif /* _ASM_LKL_BYTEORDER_H */
