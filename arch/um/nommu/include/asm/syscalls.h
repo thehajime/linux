bfb494d3d0b041a12f7c25f17853cb54ddea5415
Hajime Tazaki <thehajime@gmail.com>
2020-06-24 14:38:01 +0900

    um: nommu: system call interface and application API
    The application API is based on the kernel system call interface in
    order to offer a stable API to applications. Note that we can't
    offer the full Linux system call interface due to the limitations of
    nommu mode such as lack of virtual memory, signal, user processes, etc.
    
    The host is using dedicated kernel thread, which is switched upon the
    entry of lkl_syscall so that numbers of context switches can be reduced
    for the faster performance.
    
    To expose the API definitions to applications, this commit uses
    syscall_wrapper to define arch-specific information of syscall, and this
    is used to generate syscall_defs.h for lkl so that it can be used
    as a template of the list of syscall available for LKL apps.
    
    (TBupdated)
    To avoid collisions between the Linux API and the LKL API (e.g.  struct
    stat, MKNOD, etc.) we use a python script to modify the user headers
    and to prefix all of the global symbols (structures, typedefs,
    defines) with LKL, lkl, _LKL, _lkl, __LKL or __lkl.
    
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>
    Signed-off-by: Octavian Purdila <tavi.purdila@gmail.com>

---
+++ arch/um/nommu/include/asm/syscalls.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+int syscalls_init(void);
+void syscalls_cleanup(void);
+long lkl_syscall(long no, long *params);
+void wakeup_idle_host_task(void);
+
+#define sys_mmap sys_ni_syscall
+#define sys_rt_sigreturn sys_ni_syscall
+#define sys_arch_prctl sys_ni_syscall
+#define sys_iopl sys_ni_syscall
+#define sys_ioperm sys_ni_syscall
+#define sys_clone sys_ni_syscall
+
+int run_syscalls(void);
