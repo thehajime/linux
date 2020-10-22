f149135f1943eb81998d54c923c10ebffb0b4518
Hajime Tazaki <thehajime@gmail.com>
2020-10-22 10:05:12 +0900

    um: lkl: host interface
    This patch introduces the host operations that define the interface
    between the LKL and the host. These operations must be provided either
    by a host library or by the application itself.
    
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

---
+++ arch/um/include/asm/host_ops.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_LKL_HOST_OPS_H
+#define _ASM_LKL_HOST_OPS_H
+
+#include <uapi/asm/host_ops.h>
+
+extern struct lkl_host_operations *lkl_ops;
+
+#endif
