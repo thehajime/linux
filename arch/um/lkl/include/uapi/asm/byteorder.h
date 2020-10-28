029a149693cd4fbf953f9c177099853b6d3aa99a
Hajime Tazaki <thehajime@gmail.com>
2020-10-28 16:43:19 +0900

    um: lkl: system call interface and application API
    The application API is based on the kernel system call interface in
    order to offer a stable API to applications. Note that we can't
    offer the full Linux system call interface due to the limitations of
    library mode such as lack of virtual memory, signal, user processes, etc.
    
    The host is using dedicated kernel thread, which is switched upon the
    entry of lkl_syscall so that numbers of context switches can be reduced
    for the faster performance.
    
    To expose the API definitions to applications, this commit uses
    syscall_wrapper to define arch-specific information of syscall, and this
    is used to generate syscall_defs.h for lkl so that it can be used
    as a template of the list of syscall available for LKL apps.
    
    To avoid collisions between the Linux API and the LKL API (e.g.  struct
    stat, MKNOD, etc.) we use a python script to modify the user headers
    and to prefix all of the global symbols (structures, typedefs,
    defines) with LKL, lkl, _LKL, _lkl, __LKL or __lkl.
    
    This commit also added an exception to SPDX-License-Identifier for the
    library mode of UML, since applications may statically link the library,
    thus this the GPL exception to the exported headers doesn't apply to.
    
    Cc: Masahiro Yamada <masahiroy@kernel.org>
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>
    Signed-off-by: Octavian Purdila <tavi.purdila@gmail.com>

--- arch/um/lkl/include/uapi/asm/byteorder.h
+++ arch/um/lkl/include/uapi/asm/byteorder.h
@@ -2,10 +2,12 @@
 #ifndef __UM_LIBMODE_UAPI_BYTEORDER_H
 #define __UM_LIBMODE_UAPI_BYTEORDER_H
 
-#if defined(CONFIG_BIG_ENDIAN)
+#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
 #include <linux/byteorder/big_endian.h>
-#else
+#elif __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
 #include <linux/byteorder/little_endian.h>
+#else
+#error "cannot detect endian of the target"
 #endif
 
 #endif
