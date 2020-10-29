809102e694633d0e7732d6d8a9576995462402a1
Hajime Tazaki <thehajime@gmail.com>
2020-10-29 10:55:38 +0900

    um: host: add library mode build for ARCH=um
    This patch adds the skeleton for the host library.
    
    The host library is implementing the host operations needed by library
    mode and is split into host dependent (depends on a specific host, e.g.
    POSIX hosts) and host independent parts (will work on all supported
    hosts).
    
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>
    Signed-off-by: Octavian Purdila <tavi.purdila@gmail.com>

---
+++ tools/um/include/lkl_host.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LKL_HOST_H
+#define _LKL_HOST_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <lkl/asm/host_ops.h>
+#include <lkl.h>
+
+extern struct lkl_host_operations lkl_host_ops;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
