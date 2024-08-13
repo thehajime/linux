7dc2be26ea19174f6babb230b9980d6dc54ecc6d
Hajime Tazaki <thehajime@gmail.com>
2024-08-13 15:58:58 +0000

    um: nommu workaround for map_fixed
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- arch/um/kernel/syscall.c
+++ arch/um/kernel/syscall.c
@@ -14,6 +14,36 @@
 #include <linux/uaccess.h>
 #include <asm/unistd.h>
 
+/*
+ * XXX: copied from musl dynlink.c
+ * musl has a switch, DL_NOMMU_SUPPORT, to support nommu kernel, but it's
+ * a compilation-time switch, which isn't enabled on x86_64 platform.  so
+ * emulate the behavior of DL_NOMMU_SUPPORT here, until musl support it over
+ * a runtime-switch.
+ */
+
+static void *mmap_fixed(void *p, size_t n, int prot, int flags, int fd, off_t off)
+{
+	char *q;
+	ssize_t r;
+
+	/* Fallbacks for MAP_FIXED failure on NOMMU kernels. */
+	if (flags & MAP_ANONYMOUS) {
+		memset(p, 0, n);
+		return p;
+	}
+	if (ksys_lseek(fd, off, SEEK_SET) < 0) return -1;
+	for (q=p; n; q+=r, off+=r, n-=r) {
+		r = ksys_read(fd, q, n);
+		if (r < 0 && r != -EINTR) return -1;
+		if (!r) {
+			memset(q, 0, n);
+			break;
+		}
+	}
+	return p;
+}
+
 long old_mmap(unsigned long addr, unsigned long len,
 	      unsigned long prot, unsigned long flags,
 	      unsigned long fd, unsigned long offset)
@@ -26,6 +56,10 @@ long old_mmap(unsigned long addr, unsigned long len,
 		//printk(KERN_DEBUG "*************************************************************\n");
 		offset *= 4096;
 	}
+#ifndef CONFIG_MMU
+	if (flags & MAP_FIXED)
+		return mmap_fixed(addr, len, prot, flags, fd, offset);
+#endif
 
 	err = ksys_mmap_pgoff(addr, len, prot, flags, fd, offset >> PAGE_SHIFT);
  out:
