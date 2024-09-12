5c545a20adf15fd4e80666e9ecc1c4e07f4d12ab
Hajime Tazaki <thehajime@gmail.com>
2024-09-12 16:19:18 +0900

    x86/um: nommu: stack save/restore on vfork
    (IIUC) vfork(2) syscall used by !MMU shares memory space including
    stack between parent and child processes, and never return vfork(2) call
    until the child call execve(2) or _exit(2).  And before that timing,
    memory information of parent has a potential to be currupted by child's
    use.
    
    Ideally a child should immediately call execve(2) after the return from
    vfork(2) but this is not the case when we use hush(1) command of
    busybox.
    
    This commit add a workaround to save and restore stack within vfork(2)
    syscall in kernel side.
    
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>
    Signed-off-by: Ricardo Koller <ricarkol@google.com>

--- arch/x86/um/do_syscall_64.c
+++ arch/x86/um/do_syscall_64.c
@@ -35,9 +35,47 @@ static int os_x86_arch_prctl(int pid, int option, unsigned long *arg2)
 	return 0;
 }
 
+/*
+ * Make a copy of the stack, so the child does whatever it
+ * wants with it.  This copy is restored before exiting from
+ * this function.
+ *
+ * (IIUC) the vfork(2) doesn't need to copy stack, and share all
+ * memory including stack between parent and child. so this function
+ * isn't needed, but some programs (in our case hush, a shell for
+ * nommu env of busybox) corrupts stack between vfork(2)=>execve(2).
+ * the hush implementation looks fine (calling immediate execve(2)
+ * after vfork(2)), but corrupted.
+ *
+ * so, this workaround exists.
+ */
+static void *vfork_save_stack(void)
+{
+	unsigned char *stack_copy;
+
+	stack_copy = kzalloc(PAGE_SIZE << THREAD_SIZE_ORDER,
+			     GFP_KERNEL);
+	if (!stack_copy)
+		return NULL;
+
+	memcpy(stack_copy,
+	       (void *)current->thread.regs.regs.gp[HOST_SP],
+	       PAGE_SIZE << THREAD_SIZE_ORDER);
+
+	return stack_copy;
+}
+
+static void vfork_restore_stack(void *stack_copy)
+{
+	WARN_ON_ONCE(!stack_copy);
+	memcpy((void *)current->thread.regs.regs.gp[HOST_SP],
+	       stack_copy, PAGE_SIZE << THREAD_SIZE_ORDER);
+}
+
 __visible void do_syscall_64(struct pt_regs *regs)
 {
 	int syscall;
+	unsigned char *stack_copy = NULL;
 
 	syscall = PT_SYSCALL_NR(regs->regs.gp);
 	UPT_SYSCALL_NR(&regs->regs) = syscall;
@@ -45,6 +83,9 @@ __visible void do_syscall_64(struct pt_regs *regs)
 	pr_debug("syscall(%d) (current=%lx) (fn=%lx)\n",
 		 syscall, (unsigned long)current,
 		 (unsigned long)sys_call_table[syscall]);
+
+	if (syscall == __NR_vfork)
+		stack_copy = vfork_save_stack();
 
 	/* set fs register to the original host one */
 	os_x86_arch_prctl(0, ARCH_SET_FS, (void *)host_fs);
@@ -73,6 +114,8 @@ __visible void do_syscall_64(struct pt_regs *regs)
 		userspace(&current->thread.regs.regs,
 			current_thread_info()->aux_fp_regs);
 	}
+	if (syscall == __NR_vfork && regs->regs.gp[HOST_AX] > 0)
+		vfork_restore_stack(stack_copy);
 }
 
 #endif
