ee6b4a529491fefe7aae7fddbe8a0e4191510678
Hajime Tazaki <thehajime@gmail.com>
2024-09-17 13:43:29 +0900

    x86/um: nommu: elf loader for fdpic
    As UML supports CONFIG_MMU=n case, it has to use an alternate ELF
    loader, FDPIC ELF loader.  In this commit, we added necessary
    definitions in the arch, as UML has not been used so far.  It also
    updates Kconfig file to use BINFMT_ELF_FDPIC under !MMU environment.
    
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>
    Signed-off-by: Ricardo Koller <ricarkol@google.com>

--- arch/um/include/asm/mmu.h
+++ arch/um/include/asm/mmu.h
@@ -12,6 +12,10 @@
 typedef struct mm_context {
 	struct mm_id id;
 	struct uml_arch_mm_context arch;
+#ifdef CONFIG_BINFMT_ELF_FDPIC
+	unsigned long   exec_fdpic_loadmap;
+	unsigned long   interp_fdpic_loadmap;
+#endif
 } mm_context_t;
 
 /* Avoid tangled inclusion with asm/ldt.h */
