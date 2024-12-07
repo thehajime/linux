9d50f8e485077bd00b95937644db55cbe4ba2ace
Hajime Tazaki <thehajime@gmail.com>
2024-12-07 20:02:28 +0900

    um: nommu: seccomp syscalls hook
    This commit adds syscall hook with seccomp.
    
    Using seccomp raises SIGSYS to UML process, which is captured in the
    (UML) kernel, then jumps to the syscall entry point, __kernel_vsyscall,
    to hook the original syscall instructions.
    
    The SIGSYS signal is raised upon the execution from uml_reserved and
    high_physmem, which locates userspace memory.
    
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>
    Signed-off-by: Kenichi Yasukata <kenichi.yasukata@gmail.com>

---
+++ arch/x86/um/nommu/os-Linux/mcontext.c
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <sys/ucontext.h>
+#define __FRAME_OFFSETS
+#include <asm/ptrace.h>
+#include <sysdep/ptrace.h>
+#include <sysdep/mcontext.h>
+#include <sysdep/syscalls.h>
+
+void mc_set_sigsys_hook(mcontext_t *mc)
+{
+	mc->gregs[REG_RCX] = mc->gregs[REG_RIP];
+	mc->gregs[REG_RIP] = (unsigned long) __kernel_vsyscall;
+}
