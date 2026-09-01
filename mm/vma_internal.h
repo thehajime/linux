efefe33a16c885ed153cc82b7784b7bc07741a2f
Hajime Tazaki <thehajime@gmail.com>
2026-09-01 15:27:33 +0900

    mm: split common VMA and MMU-only internal dependencies
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- mm/vma_internal.h
+++ mm/vma_internal.h
@@ -9,48 +9,10 @@
 #ifndef __MM_VMA_INTERNAL_H
 #define __MM_VMA_INTERNAL_H
 
-#include <linux/backing-dev.h>
-#include <linux/bitops.h>
-#include <linux/bug.h>
-#include <linux/cacheflush.h>
-#include <linux/err.h>
-#include <linux/file.h>
-#include <linux/fs.h>
-#include <linux/huge_mm.h>
-#include <linux/hugetlb.h>
-#include <linux/hugetlb_inline.h>
-#include <linux/kernel.h>
-#include <linux/ksm.h>
-#include <linux/khugepaged.h>
-#include <linux/list.h>
-#include <linux/maple_tree.h>
-#include <linux/mempolicy.h>
-#include <linux/mm.h>
-#include <linux/mm_inline.h>
-#include <linux/mm_types.h>
-#include <linux/mman.h>
-#include <linux/mmap_lock.h>
-#include <linux/mmdebug.h>
-#include <linux/mmu_context.h>
-#include <linux/mutex.h>
-#include <linux/pagemap.h>
-#include <linux/perf_event.h>
-#include <linux/personality.h>
-#include <linux/pfn.h>
-#include <linux/rcupdate.h>
-#include <linux/rmap.h>
-#include <linux/rwsem.h>
-#include <linux/sched/signal.h>
-#include <linux/security.h>
-#include <linux/shmem_fs.h>
-#include <linux/swap.h>
-#include <linux/uprobes.h>
-#include <linux/userfaultfd_k.h>
-#include <linux/pgtable.h>
+#include "vma_common_internal.h"
 
-#include <asm/current.h>
-#include <asm/tlb.h>
-
-#include "internal.h"
+#ifdef CONFIG_MMU
+#include "vma_mmu_internal.h"
+#endif
 
 #endif	/* __MM_VMA_INTERNAL_H */
---
+++ mm/vma_mmu_internal.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#ifndef __MM_VMA_MMU_INTERNAL_H
+#define __MM_VMA_MMU_INTERNAL_H
+
+#include <linux/rmap.h>
+#include <linux/huge_mm.h>
+#include <linux/hugetlb.h>
+#include <linux/ksm.h>
+#include <linux/khugepaged.h>
+#include <linux/swap.h>
+#include <linux/uprobes.h>
+#include <linux/userfaultfd_k.h>
+#include <linux/pgtable.h>
+#include <asm/tlb.h>
+#include "internal.h"
+
+#endif /* __MM_VMA_MMU_INTERNAL_H */
