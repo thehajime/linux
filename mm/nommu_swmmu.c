27ce0bc4ef991c9ae17efe7a70de1fea7f8ad330
Hajime Tazaki <thehajime@gmail.com>
2026-09-08 10:10:10 +0900

    rollback swmmu

--- mm/nommu_swmmu.c
+++ mm/nommu_swmmu.c
@@ -1945,6 +1945,18 @@ static void swmmu_move_abort(struct vma_remap_struct *vrm,
 	up_write(&space->lock);
 }
 
+static unsigned long swmmu_move_mapping(struct vma_remap_struct *vrm,
+					struct pagetable_move_control *pmc)
+{
+	(void)pmc;
+
+	/*
+	 * SWMMU entry preparation is already performed by
+	 * swmmu_move_prepare().
+	 */
+	return vrm->old_len;
+}
+
 static const struct vma_mapping_ops swmmu_vma_mapping_ops = {
 	.split_prepare = swmmu_split_prepare,
 	.split_commit = swmmu_split_commit,
@@ -1959,6 +1971,7 @@ static const struct vma_mapping_ops swmmu_vma_mapping_ops = {
 	.move_prepare = swmmu_move_prepare,
 	.move_commit = swmmu_move_commit,
 	.move_abort = swmmu_move_abort,
+	.move_mapping = swmmu_move_mapping,
 };
 
 
